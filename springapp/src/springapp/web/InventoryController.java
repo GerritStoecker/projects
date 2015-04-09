86aff55a2e50ce30519ad1c80ca93f03673168a5
GerritStoecker <gerrit.stoecker@gmx.de>
2015-04-09 14:21:28 +0200

    Part 4 of the Spring tutorial partly finished.

--- springapp/src/springapp/web/InventoryController.java
+++ springapp/src/springapp/web/InventoryController.java
@@ -2,6 +2,8 @@ package springapp.web;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -12,10 +14,13 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import springapp.service.ProductManager;
 
-public class HelloController implements Controller {
+public class InventoryController implements Controller {
 
     protected final Log logger = LogFactory.getLog(getClass());
+    
+    private ProductManager productManager;
 
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -23,7 +28,15 @@ public class HelloController implements Controller {
         String now = (new Date()).toString();
         logger.info("Returning hello view without prefix with " + now);
 
-        return new ModelAndView("hello", "now", now);
+        Map<String, Object> myModel = new HashMap<String, Object>();
+        myModel.put("now", now);
+        myModel.put("products", this.productManager.getProducts());
+
+        return new ModelAndView("hello", "model", myModel);
+
     }
 
+    public void setProductManager(ProductManager productManager) {
+        this.productManager = productManager;
+    }
 }
